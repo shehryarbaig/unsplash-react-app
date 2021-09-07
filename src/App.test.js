b1f3fc8cd2e31e803d373254ed0db8a8ac391abb
shehryar.baig <shehryar.baig@arbisoft.com>
2021-09-07 18:59:25 +0500

    Adding test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,26 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { act } from "react-dom/test-utils";
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import store from "./store";
+import Routes from "./routes"
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders home page without crashing", () => {
+  const div = document.createElement("div");
+  act(() => {
+    ReactDOM.render(
+        <Provider store={store}>
+        <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+        </Provider>,
+      div
+    );
+  });
+  const unsplash = div.querySelector("p.MuiTypography-root.makeStyles-UnsplashTitle-22.MuiTypography-body1").innerHTML;
+  expect(unsplash).toBe("Unsplash");
 });
